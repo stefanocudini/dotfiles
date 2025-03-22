df65cbce8c32dfeb7385b85f937064df2c015baf
Stefano Cudini <stefano.cudini@gmail.com>
2025-03-22 15:15:50 +0100

    Update apt_packages.sh

--- apt_packages.sh
+++ apt_packages.sh
@@ -4,7 +4,7 @@ apt update && apt install \
   apt-transport-https ca-certificates \
   curl aria2 pv nano \
   git meld colordiff \
-  gimp flameshot peek \
+  gimp vlc v4l-utils flameshot peek light-themes \
   ncat nmap htop nload secure-delete shred screen tmux tree \ 
   gnome-disks cryptsetup \
   saga spatialite-bin spatialite-gui 
