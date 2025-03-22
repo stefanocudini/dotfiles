732a4b8e0d9f061bd90c5a10bdfc0de7b8849c82
Stefano Cudini <stefano.cudini@gmail.com>
2025-03-22 15:52:26 +0100

    Update apt_packages.sh

--- apt_packages.sh
+++ apt_packages.sh
@@ -4,6 +4,7 @@ apt update && apt install \
   apt-transport-https ca-certificates \
   curl aria2 pv nano \
   git meld colordiff \
+  python3-pip python3-ipython \
   gimp vlc v4l-utils flameshot peek light-themes \
   ncat nmap htop nload secure-delete shred screen tmux tree \ 
   gnome-disks cryptsetup \
